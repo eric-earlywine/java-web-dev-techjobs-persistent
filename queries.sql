906a27508638b821a19c12d83e1fd1b22fb5d882
Eric <eric.earlywine@hotmail.com>
2020-01-23 13:06:47 -0600

    Finished through Part 2. TODO: Part 3 & 4

--- queries.sql
+++ queries.sql
@@ -1,6 +1,12 @@
 ## Part 1: Test it with SQL
+ID: Integer
+Employer: varchar(255)
+Name: varchar(255)
+Skills: varchar(255)
 
+SHOW COLUMNS FROM job;
 ## Part 2: Test it with SQL
+SELECT name FROM employers WHERE location = "St. Louis City";
 
 ## Part 3: Test it with SQL
 
